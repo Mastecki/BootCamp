83d7b2226be39c2208ce5dbf5d5bc2a1ebb89288
Maciej Malek <malek.maciej@gmail.com>
2023-03-21 17:05:06 +0100

    03-backend

--- terraform/03-backend-s3/deploy.sh
+++ terraform/03-backend-s3/deploy.sh
@@ -2,7 +2,8 @@ terraform init -reconfigure -backend-config=backend.tfvars
 terraform plan -out=build.plan
 if [[ $? -eq 0 ]]
 then
-    ~/.tfenv/bin/terraform apply build.plan
+    terraform apply build.plan
+    rm -f build.plan
 else
     echo "ERROR: Unable to proceed due to plan failure"
 fi
