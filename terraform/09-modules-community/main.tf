2380df335b3fd1c9bd7dca5345b243f25ffef06f
Maciej Malek <malek.maciej@gmail.com>
2023-03-25 10:50:48 +0100

    SG

--- terraform/09-modules-community/main.tf
+++ terraform/09-modules-community/main.tf
@@ -41,3 +41,12 @@ resource aws_instance "ec2" {
   subnet_id = module.vpc.public_subnets[0]
   key_name = aws_key_pair.this.key_name
 }
+
+resource "aws_security_group_rule" "internet" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpc.default_security_group_id
+}
