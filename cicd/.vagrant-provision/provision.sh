7cc1a613aeb3fccdec48635b70f108fe0dd80b30
Karol <kolodziejczyk0@gmail.com>
2023-04-22 12:28:34 +0200

    Change jenkins key in vagrant

--- cicd/.vagrant-provision/provision.sh
+++ cicd/.vagrant-provision/provision.sh
@@ -4,7 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 echo "Installing jenkins"
 
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
